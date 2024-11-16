64ba85f124910f4eb890964cfcc5aae77439b3f6
NicoG <winnipeg.francois@gmail.com>
2024-11-16 20:19:59 +0100

    Ajout d'une zone de dessin et du font gris

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-myCanvas.width = window.innerWidth;
-myCanvas.height = window.innerHeight;
+const canvasProperties={
+	width: window.innerWidth,
+	height: window.innerHeight,
+	center: {
+		x: window.innerWidth/2,
+		y: window.innerHeight/2
+	}
+}
+
+const stageProperties={
+	width: 600,
+	height: 480,
+	left: canvasProperties.center.x-600/2,
+	top: canvasProperties.center.y - 480/2
+}
+
+myCanvas.width = canvasProperties.width;
+myCanvas.height = canvasProperties.height;
+
+const ctx = myCanvas.getContext('2d');
+ctx.fillStyle="gray";
+ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
+
+
+
+ctx.fillStyle="white";
+ctx.fillRect(stageProperties.left, stageProperties.top, stageProperties.width, stageProperties.height)

dae7c83504bc1126dc18281c8e9baeb92a6b54ec
NicoG <winnipeg.francois@gmail.com>
2024-11-16 21:40:49 +0100

    Ajout fonctionnalité dessin main levée

--- script.js
+++ script.js
@@ -21,7 +21,34 @@ const ctx = myCanvas.getContext('2d');
 ctx.fillStyle="gray";
 ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
 
-
-
 ctx.fillStyle="white";
-ctx.fillRect(stageProperties.left, stageProperties.top, stageProperties.width, stageProperties.height)
+ctx.fillRect(stageProperties.left, stageProperties.top, stageProperties.width, stageProperties.height);
+
+const path = [];
+
+myCanvas.addEventListener('pointerdown', function(e){
+	const mousePosition = {
+		x: e.offsetX,
+		y: e.offsetY
+	};
+	path.push(mousePosition);
+});
+
+myCanvas.addEventListener('pointermove', function(e){
+	const mousePosition = {
+		x: e.offsetX,
+		y: e.offsetY
+	};
+	console.log(mousePosition.x);
+	path.push(mousePosition);
+});
+
+myCanvas.addEventListener('pointerup', function(e){
+	ctx.beginPath();
+	ctx.moveTo(path[0].x, path[0].y);
+	path.forEach((point)=>{
+		ctx.lineTo(point.x, point.y)
+	});
+	ctx.stroke();
+	//drawPath();
+});

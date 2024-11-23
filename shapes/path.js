8b198d65057836ac98f7ce224c7d6add472c56e9
NicoG <winnipeg.francois@gmail.com>
2024-11-23 23:13:39 +0100

    Correction dans l'affichage de la région de click

--- shapes/path.js
+++ shapes/path.js
@@ -20,7 +20,7 @@ class Path extends Shape {
 	drawHitRegion(ctx){
 		const center = this.center? this.center:{x:0, y:0}
 		ctx.beginPath();
-		ctx.moveTo(this.points[0].x +  center.x, this.points[0].y) + center.y;
+		ctx.moveTo(this.points[0].x +  center.x, this.points[0].y + center.y);
 		for(let i = 1; i < this.points.length; i++){
 				ctx.lineTo(this.points[i].x +  center.x, this.points[i].y + center.y);
 			}

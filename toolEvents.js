e0f47f42f7edc24652d108c4fe1e4f7fae462abe
NicoG <winnipeg.francois@gmail.com>
2024-12-12 23:06:56 +0100

    Sélection multiple

--- toolEvents.js
+++ toolEvents.js
@@ -9,11 +9,14 @@ function downCallbackForSelect (e) {
 	//console.log(id);
 	const shape = shapes.find(s=>s.id==id);
 	
-	shapes.forEach((s) => (s.selected = false));
+	if(e.ctrlKey === false){
+		shapes.forEach((s) => (s.selected = false));
+	}
+	
 	drawShapes(shapes);
 
 	if(shape){
-		shape.selected = true;
+		shape.selected = !shape.selected;
 		//console.log(shape);
 		//const diff = addPoints(startPosition, subtractPoints(shape.center, startPosition));
 		const oldCenter=shape.center;
@@ -28,6 +31,7 @@ function downCallbackForSelect (e) {
 			const mouseDelta = Vector.subtract(mousePosition, startPosition);
 			shape.setCenter(Vector.add(oldCenter, mouseDelta));
 			drawShapes(shapes);
+			PropertiesPanel.updateDisplay(shapes.filter((s) => s.selected));
 			//drawShapes([...shapes, currentShape]);
 		};
 	
@@ -41,5 +45,71 @@ function downCallbackForSelect (e) {
 	
 		myCanvas.addEventListener('pointermove', moveCallback);
 		myCanvas.addEventListener('pointerup', upCallback);
+	} else {
+		selectShapeUnderRectangle(e);
 	}
+}
+
+function selectShapeUnderRectangle(e){
+	const startPosition = {
+		x: e.offsetX,
+		y: e.offsetY
+	};
+
+	let rect = document.createElement("div");
+	rect.style.position = "fixed";
+	rect.style.backgroundColor = "transparent";
+	rect.style.border = "1px dotted";
+	const htmlBody = document.querySelector("body");
+	htmlBody.appendChild(rect);
+
+	let rectMinX, rectMinY, rectMaxX, rectMaxY = 0;
+
+	const moveCallback = function(e){
+		const mousePosition = {
+			x: e.clientX,
+			y: e.clientY
+		};
+		rectMinX = Math.min(startPosition.x, mousePosition.x);
+		rectMinY = Math.min(startPosition.y, mousePosition.y);
+		const width = Math.abs(startPosition.x - mousePosition.x);
+		const height = Math.abs(startPosition.y - mousePosition.y);
+		rectMaxX = rectMinX + width;
+		rectMaxY = rectMinY + height;
+		const left = rectMinX;
+		const top = rectMinY;
+		rect.style.left = `${left}px`;
+		rect.style.top = `${top}px`;
+		rect.style.width = `${width}px`;
+		rect.style.height = `${height}px`;
+		};
+	
+	const upCallback = function (e) {
+			myCanvas.removeEventListener('pointermove', moveCallback);
+			myCanvas.removeEventListener('pointerup', upCallback);
+			rect.removeEventListener("pointerup", upCallback);
+			rect.removeEventListener("pointermove", moveCallback);
+
+			shapes.forEach((shape) => {
+				const points = shape.getPoints();
+				const minX = Math.min(...points.map((p) => p.x + shape.center.x));
+				const maxX = Math.max(...points.map((p) => p.x + shape.center.x));
+				const minY = Math.min(...points.map((p) => p.y + shape.center.y));
+				const maxY = Math.max(...points.map((p) => p.y + shape.center.y));
+
+				if(minX >= rectMinX && maxX <= rectMaxX && minY >= rectMinY && maxY <= rectMaxY){
+					shape.selected = true;
+				}
+			});
+
+			rect.remove();
+			PropertiesPanel.updateDisplay(shapes.filter((s) => s.selected));
+			drawShapes(shapes);
+			updateHistory(shapes);
+		};
+	// adding event listeners to the rectangle to allow rectangle redraw when pointer moves into it
+	myCanvas.addEventListener('pointermove', moveCallback);
+	myCanvas.addEventListener('pointerup', upCallback);
+	rect.addEventListener("pointermove", moveCallback);
+	rect.addEventListener("pointerup", upCallback);
 }

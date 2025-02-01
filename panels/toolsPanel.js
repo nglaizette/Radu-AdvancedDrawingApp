d0e2254c08f76b60ffa567886241fe22720bd7c1
NicoG <winnipeg.francois@gmail.com>
2025-02-01 16:21:46 +0100

    Ajout des bouton load, save and Export

--- panels/toolsPanel.js
+++ panels/toolsPanel.js
@@ -1,47 +1,58 @@
 class ToolsPanel {
 	constructor(holderDiv){
 		this.holderDiv = holderDiv;
-		this.holderDiv.innerHTML = "Shape tools";
-			this.holderDiv.appendChild(
-				createInputWithLabel("Path", {
-					type: "radio",
-					id: "pathTool",
-					name: "shapeTools",
-				}),
-			);
 
-			this.holderDiv.appendChild(
-				createInputWithLabel("Rect", {
-					type: "radio",
-					id: "rectTool",
-					name: "shapeTools",
-				}),
-			);
+		this.holderDiv.appendChild(createDOMElement("div", {id: "documentToolsHeader"}, "Document"));
 
-			this.holderDiv.appendChild(
-				createInputWithLabel("Oval", {
-					type: "radio",
-					id: "ovalTool",
-					name: "shapeTools",
-				}),
-			);
+		this.holderDiv.appendChild(createDOMElement("button", {id: "saveBtn"}, "Save"));
+		this.holderDiv.appendChild(createDOMElement("br"));
+		this.holderDiv.appendChild(createDOMElement("button", {id: "loadBtn"}, "Load"));
+		this.holderDiv.appendChild(createDOMElement("br"));
+		this.holderDiv.appendChild(createDOMElement("button", {id: "exportBtn"}, "Export"));
 
-			this.holderDiv.appendChild(
-				createInputWithLabel("Text", {
-					type: "radio",
-					id: "textTool",
-					name: "shapeTools",
-				}),
-			);
+		this.holderDiv.appendChild(createDOMElement("hr"));
 
-			this.holderDiv.appendChild(
-				createInputWithLabel("Select", {
-					type: "radio",
-					id: "selectTool",
-					name: "shapeTools",
-				}),
-			);
+		this.holderDiv.appendChild(createDOMElement("div", {id: "shapeToolsHeader"}, "Shape tools"));
+		this.holderDiv.appendChild(
+			createInputWithLabel("Path", {
+				type: "radio",
+				id: "pathTool",
+				name: "shapeTools",
+			}),
+		);
 
-			this.holderDiv.appendChild(createDOMElement("hr"));
+		this.holderDiv.appendChild(
+			createInputWithLabel("Rect", {
+				type: "radio",
+				id: "rectTool",
+				name: "shapeTools",
+			}),
+		);
+
+		this.holderDiv.appendChild(
+			createInputWithLabel("Oval", {
+				type: "radio",
+				id: "ovalTool",
+				name: "shapeTools",
+			}),
+		);
+
+		this.holderDiv.appendChild(
+			createInputWithLabel("Text", {
+				type: "radio",
+				id: "textTool",
+				name: "shapeTools",
+			}),
+		);
+
+		this.holderDiv.appendChild(
+			createInputWithLabel("Select", {
+				type: "radio",
+				id: "selectTool",
+				name: "shapeTools",
+			}),
+		);
+
+		this.holderDiv.appendChild(createDOMElement("hr"));
 	}
 } 

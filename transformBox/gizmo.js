49d0e91cc42344676741ab3b9f61f956c402fff8
NicoG <winnipeg.francois@gmail.com>
2025-02-22 22:27:17 +0100

    Ajout de la containte de garder l'échelle de l'objet à dessier lorsqu'on appuie sur shift

--- transformBox/gizmo.js
+++ transformBox/gizmo.js
@@ -1,110 +1,126 @@
 class Gizmo {
-
-	constructor(shape){
+	
+	constructor(shape) {
 		this.shape = shape;
 
 		this.center = this.shape.center;
-		const points = this.shape.getPoints();
-		this.box = BoundingBox.fromPoints(points.map((p) => p.add(this.center)));
+		this.box = BoundingBox.fromPoints(
+			this.shape.getPoints().map((p) => p.add(this.center))
+		);
+
 		this.handles = [
 			new Handle(this.box.topLeft, Handle.TOP_LEFT),
 			new Handle(this.box.topRight, Handle.TOP_RIGHT),
 			new Handle(this.box.bottomLeft, Handle.BOTTOM_LEFT),
 			new Handle(this.box.bottomRight, Handle.BOTTOM_RIGHT),
-			new Handle(Vector.midVector([this.box.topLeft, this.box.topRight]), Handle.TOP),
-			new Handle(Vector.midVector([this.box.topRight, this.box.bottomRight]), Handle.RIGHT),
-			new Handle(Vector.midVector([this.box.bottomRight, this.box.bottomLeft]), Handle.BOTTOM),
-			new Handle(Vector.midVector([this.box.bottomLeft, this.box.topLeft]), Handle.LEFT),
+			new Handle(
+				Vector.midVector([this.box.topLeft, this.box.topRight]),
+				Handle.TOP
+			),
+			new Handle(
+				Vector.midVector([this.box.bottomLeft, this.box.bottomRight]),
+				Handle.BOTTOM
+			),
+			new Handle(
+				Vector.midVector([this.box.topLeft, this.box.bottomLeft]),
+				Handle.LEFT
+			),
+			new Handle(
+				Vector.midVector([this.box.topRight, this.box.bottomRight]),
+				Handle.RIGHT
+			),
 		];
-			
 	}
 
 	hasHandle(id) {
-		return this.handles.find((handle) => handle.id === id);
+		return this.handles.find((handle) => handle.id == id);
 	}
 
 	addEventListeners(startPosition, handle, selectedShapes) {
-		const oldBoxes = selectedShapes.map((s) => 
-			BoundingBox.fromPoints(s.getPoints().map((p) => p.add(this.center))));
+		const oldBoxes = selectedShapes.map((s) =>
+			BoundingBox.fromPoints(s.getPoints().map((p) => p.add(this.center)))
+		);
 		let mouseDelta = null;
 		let isDragging = false;
-		const moveCallback = e => {
-			const mousePosition = new Vector(e.offsetX,e.offsetY);
+		const moveCallback = (e) => {
+			const mousePosition = new Vector(e.offsetX, e.offsetY);
 			const diff = Vector.subtract(mousePosition, startPosition);
 			mouseDelta = viewport.scale(diff);
 			isDragging = true;
 
-			const ratio = new Vector(
+			let ratio = new Vector(
 				mouseDelta.x / this.box.width,
 				mouseDelta.y / this.box.height
-			).scale(2)
-			.add(new Vector(1, 1));
+			)
+				.scale(2)
+				.add(new Vector(1, 1));
 
-			for(let i = 0; i < selectedShapes.length; i++) {
+			switch (handle.type) {
+				case Handle.RIGHT:
+					ratio = new Vector(ratio.x, 1);
+					break;
+				case Handle.LEFT:
+					ratio = new Vector(2 - ratio.x, 1);
+					break;
+				case Handle.TOP:
+					ratio = new Vector(1, 2 - ratio.y);
+					break;
+				case Handle.BOTTOM:
+					ratio = new Vector(1, ratio.y);
+					break;
+				case Handle.TOP_LEFT:
+					ratio = new Vector(2 - ratio.x, 2 - ratio.y);
+					break;
+				case Handle.TOP_RIGHT:
+					ratio = new Vector(ratio.x, 2 - ratio.y);
+					break;
+				case Handle.BOTTOM_LEFT:
+					ratio = new Vector(2 - ratio.x, ratio.y);
+					break;
+				case Handle.BOTTOM_RIGHT:
+					ratio = new Vector(ratio.x, ratio.y);
+					break;
+			}
+
+			// Preserve aspect ratio if shift key is held
+			// region shift key preserve ratio
+			if (
+				e.shiftKey &&
+				[
+					Handle.TOP_LEFT,
+					Handle.TOP_RIGHT,
+					Handle.BOTTOM_LEFT,
+					Handle.BOTTOM_RIGHT,
+				].includes(handle.type)
+			) {
+				const scaler = Math.max(Math.abs(ratio.x), Math.abs(ratio.y));
+				ratio = new Vector(
+					Math.sign(ratio.x) * scaler,
+					Math.sign(ratio.y) * scaler
+				);
+			}
+
+			// endregion
+			for (let i = 0; i < selectedShapes.length; i++) {
 				const shape = selectedShapes[i];
 				const oldBox = oldBoxes[i];
 
-				switch(handle.type){
-					case Handle.RIGHT:
-						shape.changeWidth(oldBox.width, ratio.x);
-						break;
-					case Handle.LEFT:
-						shape.changeWidth(oldBox.width, 2 - ratio.x);
-						break;
-					case Handle.TOP:
-						shape.changeHeight(oldBox.height, 2 - ratio.y);
-						break;
-					case Handle.BOTTOM:
-						shape.changeHeight(oldBox.height, ratio.y);
-						break;
-					case Handle.TOP_LEFT:
-						shape.changeSize(
-							oldBox.width,
-							oldBox.height,
-							2 - ratio.x,
-							2 - ratio.y);
-						break;
-					case  Handle.TOP_RIGHT:
-						shape.changeSize(
-							oldBox.width,
-							oldBox.height,
-							ratio.x,
-							2 - ratio.y
-							);
-						break;
-					case Handle.BOTTOM_LEFT:
-						shape.changeSize(
-							oldBox.width,
-							oldBox.height,
-							2 - ratio.x,
-							ratio.y
-						);
-						break;
-					case Handle.BOTTOM_RIGHT:
-						shape.changeSize(
-							oldBox.width,
-							oldBox.height,
-							ratio.x,
-							ratio.y
-						);
-						break;
-				}
+				shape.changeSize(oldBox.width, oldBox.height, ratio.x, ratio.y);
 			}
+
 			viewport.drawShapes(shapes);
 			PropertiesPanel.updateDisplay(selectedShapes);
 		};
-	
-		const upCallback = e => {
-			viewport.canvas.removeEventListener('pointermove', moveCallback);
-			viewport.canvas.removeEventListener('pointerup', upCallback);	
-		}
-	
-		viewport.canvas.addEventListener('pointermove', moveCallback);
-		viewport.canvas.addEventListener('pointerup', upCallback);
+
+		const upCallback = (e) => {
+			viewport.canvas.removeEventListener("pointermove", moveCallback);
+			viewport.canvas.removeEventListener("pointerup", upCallback);
+		};
+		viewport.canvas.addEventListener("pointermove", moveCallback);
+		viewport.canvas.addEventListener("pointerup", upCallback);
 	}
 
-	draw(ctx, hitRegion = false){
-
+	draw(ctx, hitRegion = false) {
 		ctx.save();
 		ctx.beginPath();
 		ctx.rect(
@@ -125,12 +141,12 @@ class Gizmo {
 		ctx.beginPath();
 		ctx.lineWidth = 2 / viewport.zoom;
 		ctx.setLineDash([1, 1]);
-		ctx.arc(this.center.x, this.center.y, size / 2.0, 0, 2 * Math.PI);
+		ctx.arc(this.center.x, this.center.y, size / 2, 0, 2 * Math.PI);
 		ctx.stroke();
 		ctx.lineWidth = 1 / viewport.zoom;
 		ctx.setLineDash([]);
 
-		for(const handle of this.handles){
+		for (const handle of this.handles) {
 			handle.draw(ctx, hitRegion);
 		}
 
